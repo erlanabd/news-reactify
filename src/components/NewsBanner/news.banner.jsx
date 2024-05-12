86606e5bd006ba4435ddd757a1f8908a54347474
Erlan <eabdyrakhmanovv@gmail.com>
2024-05-12 21:27:17 +0600

    fix

--- src/components/NewsBanner/news.banner.jsx
+++ src/components/NewsBanner/news.banner.jsx
@@ -7,7 +7,6 @@ const NewsBanner = ({ item }) => {
   return (
     <div className={styles.banner}>
       <Image image={item.image} />
-      <Image image={item.image} />
       <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
         {formatTimeAgo(item.published)} by {item.author}
