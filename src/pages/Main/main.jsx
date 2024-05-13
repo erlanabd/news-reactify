a1808f8cdb0a57cc366aa6b4c648bcc5d6c0a075
Erlan <eabdyrakhmanovv@gmail.com>
2024-05-13 23:36:05 +0600

    add search input and debounce hook

--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -6,15 +6,20 @@ import NewsList from "../../components/NewsList/news.list";
 import Skeleton from "../../components/Skeleton/skeleton";
 import Pagination from "../../components/Pagination/Pagination";
 import Categories from "../../components/Categories/categories";
+import Search from "../../components/Search/search";
+import { useDebounce } from "../../helpers/hooks/useDebounce";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [categories, setCategories] = useState([]);
+  const [keywords, setKeywords] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All");
   const totalPages = 10;
   const pageSize = 10;
+
+  const debounceKeywords = useDebounce(keywords, 1500);
 
   const fetchNews = async (currentPage) => {
     try {
@@ -23,6 +28,7 @@ const Main = () => {
         page_number: currentPage,
         page_size: pageSize,
         category: selectedCategory === "All" ? null : selectedCategory,
+        keywords: debounceKeywords,
       });
       setNews(response.news);
       setIsLoading(false);
@@ -40,14 +46,13 @@ const Main = () => {
     }
   };
 
-
   useEffect(() => {
     fetchCategories();
   }, []);
 
   useEffect(() => {
     fetchNews(currentPage);
-  }, [currentPage, selectedCategory]);
+  }, [currentPage, selectedCategory, debounceKeywords]);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -65,6 +70,7 @@ const Main = () => {
     setCurrentPage(pageNumber);
   };
 
+
   return (
     <main className={styles.main}>
       <Categories
@@ -72,6 +78,8 @@ const Main = () => {
         setSelectedCategory={setSelectedCategory}
         selectedCategory={selectedCategory}
       />
+      <Search keywords={keywords} setKeywords={setKeywords} />
+
       {news.length > 0 && !isLoading ? (
         <NewsBanner item={news[0]} />
       ) : (
