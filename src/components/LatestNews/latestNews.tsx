b9169bf3d6b5c17f94b96e5fd8866c147a68514b
Erlan <eabdyrakhmanovv@gmail.com>
2024-05-21 03:50:10 +0600

    add rtk query and redux toolkit

--- src/components/LatestNews/latestNews.tsx
+++ src/components/LatestNews/latestNews.tsx
@@ -1,11 +1,9 @@
-import { getLatestNews } from "../../api/apiNews";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { NewsApiResponse } from "../../interfaces";
+import { useGetLatestNewsQuery } from "../../store/services/newsApi";
 import BannersList from "../BannersList/bannersList";
 import styles from "./styles.module.scss";
 
 const LatestNews = () => {
-  const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNews);
+  const { data, isLoading } = useGetLatestNewsQuery(null);
   return (
     <section className={styles.section}>
       <BannersList banners={data && data.news} isLoading={isLoading} />
